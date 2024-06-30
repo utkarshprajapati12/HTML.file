95d0a017425ecb4af7d0449890445099c855ad89
utkarshprajapati12 <UTKARSHPRAJAPATI5000@GMAIL.COM>
2024-06-30 14:43:24 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+
+var typed = new Typed(".text"
+    strings:["Frontend Developer", "Web Developer", "Full Stack Developer"],
+    typeSpeed : 10,
+    backSpeed : 100,
+    backDelay : 1000,
+    loop : true
+});
